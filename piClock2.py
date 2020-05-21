4d0d8553929237809814d87888370cbd4144c44e
mbmas01 <65053828+mbmas01@users.noreply.github.com>
2020-05-20 20:00:03 -0400

    Update piClock2.py

--- piClock2.py
+++ piClock2.py
@@ -1,12 +1,12 @@
 from os import walk
 # import numpy
 import socket
-from tkinter import * 
-#from tkinter.ttk import *
-from time import strftime 
+from tkinter import *
+# from tkinter.ttk import *
+from time import strftime
 from PIL import ImageTk, Image
 from itertools import cycle
-import time # TODO: clean up imports
+import time  # TODO: clean up imports
 from random import seed
 from random import randint
 
@@ -18,17 +18,15 @@ from random import randint
 timeCheck = 0
 seed(1)
 
-
 # creating tkinter window
 root = Tk()
 w = root.winfo_screenwidth()  # width for the Tk root
-h = 600 # height for the Tk root
-DigitMaxSize = (round(w / 4,0), round(h,0)) #maximum size of a given digit
+h = 600  # height for the Tk root
+DigitMaxSize = (round(w / 4, 0), round(h, 0))  # maximum size of a given digit
 
 # get screen width and height
 ws = root.winfo_screenwidth()
 hs = root.winfo_screenheight()
-
 
 # calculate x and y coordinates for the Tk root window
 x = 0
@@ -38,7 +36,7 @@ y = 0
 root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
 # for removing borders on window
-root.overrideredirect(True) 
+root.overrideredirect(True)
 
 # create frame within root window to hold clock images
 clockFrame = Frame(root)
@@ -48,7 +46,7 @@ clockFrame.pack()
 canvas = Canvas(clockFrame, bg="black", width=2048, height=600, highlightthickness=0)
 
 # default image path
-Picturemode = 0 #Picture Types, 0 = Pictures with Numbers, 1 = Pictures without Numbers
+Picturemode = 0  # Picture Types, 0 = Pictures with Numbers, 1 = Pictures without Numbers
 BackgroundPath = "./images/backgrounds"
 NumberPath = "./images/Numbers"
 NumberedPhotosPath = "./images/NumberedPhotos"
@@ -57,7 +55,7 @@ NumberedPhotosPath = "./images/NumberedPhotos"
 f = []
 for (dirpath, dirnames, filenames) in walk(BackgroundPath):
     f.extend(filenames)
-    break    
+    break
 print(dirnames)
 
 f = []
@@ -78,7 +76,6 @@ NumberedstyleCycle = cycle(NumberedstyleList)
 # default style to first one
 Numberedstyle = next(NumberedstyleCycle)
 
-
 # initialize images to default style
 canvas.image0 = ImageTk.PhotoImage(Image.open("./images/Numbers/0.png"))
 canvas.image1 = ImageTk.PhotoImage(Image.open("./images/Numbers/0.png"))
@@ -89,34 +86,36 @@ canvas.image5 = ImageTk.PhotoImage(Image.open("./images/Numbers/0.png"))
 
 # put images in list for easier modification later
 imageList = [canvas.image0,
-            canvas.image1,
-            canvas.image2,
-            canvas.image3,
-            canvas.image4,
-            canvas.image5]
+             canvas.image1,
+             canvas.image2,
+             canvas.image3,
+             canvas.image4,
+             canvas.image5]
 
 # put images in Backgroun list for easier modification later
 junk = [canvas.image0,
-            canvas.image1,
-            canvas.image2,
-            canvas.image3,
-            canvas.image4,
-            canvas.image5]
+        canvas.image1,
+        canvas.image2,
+        canvas.image3,
+        canvas.image4,
+        canvas.image5]
 
-# Function to display hostname and 
+
+# Function to display hostname and
 # IP address 
-def get_Host_name_IP(): 
-    try: 
-        host_name = socket.gethostname() 
-        host_ip = socket.gethostbyname(host_name) 
-        print("Hostname :  ",host_name) 
-        print("IP : ",host_ip) 
-    except: 
+def get_host_name_ip():
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        print("Hostname :  ", host_name)
+        print("IP : ", host_ip)
+        print("Screen Size: ", ws, ",", hs)
+    except:
         print("Unable to get Hostname and IP")
 
 
 # get image corresponding to number - This routine used for photos that already have numbers, e.g. NBA jerseys (Mode 1)
-def getNumberPath(number):
+def getnumberpath(number):
     switcher = {}
     for i in range(10):
         switcher[str(i)] = NumberedPhotosPath + "/" + Numberedstyle + "/" + str(i) + ".jpg"
@@ -126,93 +125,97 @@ def getNumberPath(number):
 
 
 # get image corresponding to number - This routine used to get numbers for photos without numbers, e.g. Family Pics (Mode 0)
-def getNumberImagePath(number):
+def getnumberimagepath(number):
     switcher = {}
     for i in range(10):
         # BackgroundImageList = imagePath + style + "/" + str(i) + "BackgroundImageList.jpg"
         switcher[str(i)] = NumberPath + "/" + str(i) + ".png"
         img = Image.open(NumberPath + "/" + str(i) + ".png")
-        
+
     return switcher.get(number, "invalid image number")
+
 
 # get image corresponding to number
 def getBackgroundImagePath(number):
     switcher = {}
 
     f = []
-    for (dirpath, dirnames, filenames) in walk(BackgroundPath+"/"+style):
+    for (dirpath, dirnames, filenames) in walk(BackgroundPath + "/" + style):
         f.extend(filenames)
-        break    
-    # print(filenames)
+        break
+        # print(filenames)
     # print(len(filenames))
-    
+
     for i in range(10):
-        switcher[str(i)] = BackgroundPath + "/" + style + "/" + filenames[randint(0,len(filenames)-1)]
-       
+        switcher[str(i)] = BackgroundPath + "/" + style + "/" + filenames[randint(0, len(filenames) - 1)]
+
     return switcher.get(number, "invalid image number")
 
 
-def refreshMode0Time():
+def refreshmode0time():
     canvas.delete("all")
     timeString = strftime("%I%M%S")
 
     xPos = 0
     for i in range(4):
-        imageList[i] = Image.open(getNumberPath(timeString[i]))
+        imageList[i] = Image.open(getnumberpath(timeString[i]))
         imageList[i].thumbnail(DigitMaxSize)
         imageList[i] = ImageTk.PhotoImage(imageList[i])
         canvas.create_image(xPos, 0, image=imageList[i], anchor='nw')
-        xPos += round(w/4,0)
+        xPos += round(w / 4, 0)
 
 
 # clears canvas and refreshes the time and time images
-def refreshMode1Time():
+def refreshmode1time():
     global img
     global junk
     global w
 
     canvas.delete("all")
     timeString = strftime("%I%M%S")
-   
+
     xPos = 0
     percent = 1
     for i in range(4):
         # Open the BackgroundImageList Image - fixed to "BackgroundImageList" for now
         # Get the BackgroundImageList dimensions - should be 512 x 600
         BackgroundImage = (Image.open(getBackgroundImagePath(timeString[i])))
-        
+
         BackgroundImage.thumbnail(DigitMaxSize)
         bwidth, bheight = BackgroundImage.size
         # print(BackgroundImage.size)
         # Get the dimensions of the foreground image (the digit)
-        imageList[i] = (Image.open(getNumberImagePath(timeString[i])))
+        imageList[i] = (Image.open(getNumberimagepath(timeString[i])))
         imwidth, imheight = imageList[i].size
         # Paste the foreground onto the BackgroundImageList
-        BackgroundImage.paste(imageList[i], (int(bwidth/2-imwidth/2),bheight - imheight - 10), imageList[i])
-        junk[i]=ImageTk.PhotoImage(BackgroundImage)
-        #canvas.create_image(xPos, 0, image=junk, anchor='nw')
-        canvas.create_image(xPos,600,image=junk[i], anchor='sw')
-        xPos += round(w/4,0)
+        BackgroundImage.paste(imageList[i], (int(bwidth / 2 - imwidth / 2), bheight - imheight - 10), imageList[i])
+        junk[i] = ImageTk.PhotoImage(BackgroundImage)
+        # canvas.create_image(xPos, 0, image=junk, anchor='nw')
+        canvas.create_image(xPos, 600, image=junk[i], anchor='sw')
+        xPos += round(w / 4, 0)
         # print ("xPos:",xPos)
-        
+
+
 # main clock function loop which refreshes every 1 sec
-def clock(): 
+def clock():
     global timeCheck
     if Picturemode == 0:
-        refreshMode0Time()
+        refreshmode0time()
     else:
-        refreshMode1Time()
+        refreshmode1time()
     canvas.after(5000, clock)
 
+
 # enable changing of Picture Modes
-def changePictureMode():
+def changepicturemode():
     global Picturemode
     Picturemode += 1
     if Picturemode > 1: Picturemode = 0
     if Picturemode == 0:
-        refreshMode0Time()
+        refreshmode0time()
     else:
-        refreshMode1Time()
+        refreshmode1time()
+
 
 # enable changing of styles
 def changeStyle():
@@ -221,23 +224,25 @@ def changeStyle():
     style = next(styleCycle)
     Numberedstyle = next(NumberedstyleCycle)
     if Picturemode == 0:
-        refreshMode0Time()
+        refreshmode0time()
     else:
-        refreshMode1Time()
+        refreshmode1time()
+
 
 ########################### MENU SYSTEM ###########################
 
-def quit_program ():
+def quit_program():
     root.destroy()
 
+
 def close_menu():
-    menu.destroy()
+    openMenu.destroy()
+
 
 def openMenu(event):
     # menu will be another canvas on top of the clockFrame canvas
-    menuCanvas = Canvas(clockFrame, bg="white", width=w, height=600, highlightthickness=0)
+    menuCanvas = Canvas(clockFrame, bg="white", width=w, height=int(1200), highlightthickness=0)
     # menuLabel = menuCanvas.create_text(512, 40, fill="black", justify="center", font="Arial 28", text="Menu")
-
 
     # picture mode
     # TODO: will toggle modes such as..
@@ -246,16 +251,18 @@ def openMenu(event):
 
     # place menu and buttons
     # TODO: change buttons from Canvases to rectangle objects? might make more sense
-    menuCanvas.place(x = 0, y = 0)
+    menuCanvas.place(x=0, y=0)
 
-    button1 = Button(menuCanvas, text = "Quit Program", command = quit_program, height=5, width=100)
+    button1 = Button(menuCanvas, text="Quit Program", command=quit_program, height=5, width=100)
     button2 = Button(menuCanvas, text="Close Menu", command=close_menu, height=5, width=100)
     button3 = Button(menuCanvas, text="Change Photo Style", command=changeStyle, height=5, width=100)
-    button4 = Button(menuCanvas, text="Change Photo Mode", command=changePictureMode,height=5,  width=100)
+    button4 = Button(menuCanvas, text="Change Photo Mode", command=changepicturemode, height=5, width=100)
     button1.grid(row=1, column=1)
     button2.grid(row=2, column=1)
     button3.grid(row=3, column=1)
     button4.grid(row=4, column=1)
+
+
 #    button1.pack(fill=BOTH, expand=1)
 #    button2.pack(fill=BOTH, expand=1)
 #    button3.pack(fill=BOTH, expand=1)
@@ -265,13 +272,13 @@ def openMenu(event):
 canvas.bind("<ButtonPress-1>", openMenu)
 ########################### END MENU SYSTEM ###########################
 
-canvas.pack(side = BOTTOM)
+canvas.pack(side=BOTTOM)
 
 # run time function
 clock()
 
 # Driver code
-get_Host_name_IP() #Function call 
+get_host_name_ip()  # Function call
 
 # infinite running loop
-root.mainloop() 
+root.mainloop()
